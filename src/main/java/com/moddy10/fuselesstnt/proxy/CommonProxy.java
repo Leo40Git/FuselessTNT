aea1eb9d009aebd11aebd89e153d3ccd0e3e4830
Leo40Git <kfir40mailminer@gmail.com>
2017-08-05 17:49:24 +0300

    Removed config, finish Forge Update Check

--- src/main/java/com/moddy10/fuselesstnt/proxy/CommonProxy.java
+++ src/main/java/com/moddy10/fuselesstnt/proxy/CommonProxy.java
@@ -1,17 +1,11 @@
 package com.moddy10.fuselesstnt.proxy;
 
 import com.moddy10.fuselesstnt.FuselessTNT;
-import com.moddy10.fuselesstnt.ModConfig;
 import com.moddy10.fuselesstnt.blocks.BlockTNTFuseless;
 import com.moddy10.fuselesstnt.init.ModBlocks;
-import com.moddy10.fuselesstnt.init.ModItems;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.SoundType;
-import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
-import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -23,19 +17,13 @@ import net.minecraftforge.registries.IForgeRegistry;
 @Mod.EventBusSubscriber
 public class CommonProxy {
 
-	public static Configuration config;
-
 	public void preInit(FMLPreInitializationEvent e) {
-		config = new Configuration(e.getSuggestedConfigurationFile());
-		ModConfig.readConfig();
 	}
 
 	public void init(FMLInitializationEvent e) {
 	}
 
 	public void postInit(FMLPostInitializationEvent e) {
-		if (config.hasChanged())
-			config.save();
 	}
 
 	@SubscribeEvent
